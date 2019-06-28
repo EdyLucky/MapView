2036e48c51308f416cdd16aca1b9a5e33bf175a3
Edy <elshad040@gmail.com>
2019-06-28 11:26:27 +0400

    How to create Map view in SwiftUI?

--- MapView/ContentView.swift
+++ MapView/ContentView.swift
@@ -7,10 +7,22 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct ContentView : View {
-    var body: some View {
-        Text("Hello World")
+struct ContentView : UIViewRepresentable {
+    
+    // creates and returns an empty
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    // sets the map view’s region to the correct coordinates to center the map on Apple’s headquarters
+    func updateUIView(_ view: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 37.3318, longitude: -122.0312)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        view.setRegion(region, animated: true)
     }
 }
 
@@ -21,3 +33,11 @@ struct ContentView_Previews : PreviewProvider {
     }
 }
 #endif
+
+
+
+
+
+
+
+
